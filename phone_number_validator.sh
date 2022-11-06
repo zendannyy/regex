04bc44a65704a71717f515191f56b96711cbe728
zendannyy <dannyzen024@gmail.com>
2022-11-05 22:54:28 -0700

    phone number validator for shell

---
+++ phone_number_validator.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# shebang is for UNix systems, so users PATH is respected
+
+set -e
+_B='\033[1m'
+_R='\033[0;31m'
+_G='\033[0;32m'
+_Z='\033[0m'
+
+# These RegExes are designed for a phone number
+regex1="^\d[-]|(\(\d{3}\) ?)|(\d{3}-)?\d{3}-\d{4}$"
+
+
+if [ -z $1 ]; then
+    echo Usage: $0 \<string to match\>
+    exit
+fi
+echo -ne ${_Z}
+echo Trying to match [$1]
+
+# validator
+[[ $1 =~ $regex1 ]] && \
+    echo -e \* [${_B}$1${_Z}] ${_G}matches regexp${_Z}: $regex1 || \
+    echo -e \* [${_B}$1${_Z}] ${_R}DOES NOT MATCH${_Z}: $regex1
+
