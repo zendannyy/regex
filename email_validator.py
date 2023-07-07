991cde2dbe46ac419608b214401014ab770bec7b
zendannyy <dannyzen024@gmail.com>
2023-07-06 21:59:04 -0700

    wrap into a function

--- email_validator.py
+++ email_validator.py
@@ -3,18 +3,20 @@ import re
 import email.utils as eu
 import sys 
 
-# check if input is a valid email
-#  print name and email
-rex = '<[A-Za-z](\w|-|\.|_)+@[A-Za-z]+\.[A-Za-z]{1,3}>'
-for i in range(int(input())):
+def email_match(name, email):
+    """check if input is a valid email
+    print name and email"""
+    rex = '<[A-Za-z](\w|-|\.|_)+@[A-Za-z]+\.[A-Za-z]{1,3}>'
+    # for i in range(int(input())):
     name, email = eu.parseaddr(input())
-    if rex.match(email):
+    if re.match(rex,email):
         print(eu.formataddr(name, email))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) <3:
         sys.exit("Usage: python3 email_validator.py name email")
     name = ''.join(sys.argv[1:])
     email = ''.join(sys.argv[2:])
-    print(name, email)
+    email_match(name, email)
+
