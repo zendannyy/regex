b24044608f7c32dedbd4496c9f6bc57fff1b1e10
zendannyy <dannyzen024@gmail.com>
2023-07-16 19:09:56 -0700

    fixed bug with taking input

--- email_validator.py
+++ email_validator.py
@@ -1,22 +1,21 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
 import re
 import email.utils as eu
 import sys 
 
-def email_match(name, email):
+def email_match(email):
     """check if input is a valid email
     print name and email"""
-    rex = '<[A-Za-z](\w|-|\.|_)+@[A-Za-z]+\.[A-Za-z]{1,3}>'
-    # for i in range(int(input())):
-    name, email = eu.parseaddr(input())
-    if re.match(rex,email):
-        print(eu.formataddr(name, email))
+    rex = r'[A-Za-z](\w|-|\.|_)+@[A-Za-z]+\.[A-Za-z]{1,3}'
+    # name, email = eu.parseaddr(input())
+    if re.match(rex, email):
+        print(eu.parseaddr((email)))
+    else:
+        sys.exit("That is an invalid email address")
 
 
 if __name__ == '__main__':
-    if len(sys.argv) <3:
+    if len(sys.argv) <2:
         sys.exit("Usage: python3 email_validator.py name email")
-    name = ''.join(sys.argv[1:])
-    email = ''.join(sys.argv[2:])
-    email_match(name, email)
-
+    email = sys.argv[1]
+    # email = sys.argv[2]
+    email_match(email)
