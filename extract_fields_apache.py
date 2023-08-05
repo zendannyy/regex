52dae82eb61355e74026bf86abbe704b917f9c09
zendannyy <dannyzen024@gmail.com>
2023-08-04 21:32:17 -0700

    adding FileNotFound check

--- extract_fields_apache.py
+++ extract_fields_apache.py
@@ -1,31 +1,40 @@
 import argparse 
+import errno
 import re
 import sys
 
 def ip_match(logfile):
     """id and extract IP's"""
     rex = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
-
-    with open('logfile.txt', 'r') as f:
-        for line in f:
-            try:
-                # if re.search(rex, line):
-                print(re.search(rex, line).group())
-            except AttributeError:
-                sys.exit("no IP found")
-        print('\n')
+    print(f'The following IP addresses and Status Codes were found in the logfile \n')
+    
+    try:
+        with open(logfile, 'r') as f:
+            for line in f:
+                if re.search(rex, line):
+                    print(re.search(rex, line).group())
+    except FileNotFoundError as fe:
+        if fe.errno == errno.ENOENT:
+            print(f"File '{logfile}' does not exist")
+    except AttributeError:
+        sys.exit("no IP found")
+    print('\n')
 
 
 def status_match(logfile):
     "id and extract status codes"
     rex = "\s[2-5]\d{2}"
-    with open('logfile.txt', 'r') as f:
-        for line in f:
-            try:
+    try:
+        with open(logfile, 'r') as f:
+            for line in f:
                 # if re.search(rex, line):
                 print(re.search(rex, line).group())
-            except AttributeError:
-                sys.exit("no Status Code found")
+    except FileNotFoundError as fe:
+         if fe.errno == errno.ENOENT:
+            print(f"File '{logfile}' does not exist")
+    except AttributeError:
+        sys.exit("no Status Code found")
+
 
 def main():
     parser = argparse.ArgumentParser(description='Read log file, parse & extract IP addresses.')
