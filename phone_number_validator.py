1c8c409b152cc8bf4a6dd4c74cce4eda9b59660b
zendannyy <dannyzen024@gmail.com>
2025-08-15 22:15:59 -0700

    add argparser, csv functionality

--- phone_number_validator.py
+++ phone_number_validator.py
@@ -1,25 +1,42 @@
+import argparse
 import csv
 import re
+import sys
 from re import findall
 
-def phone_match():
-    """Take phone numbers as input 
-
+def phone_match(csv_file):
+    """Take a csv with phone numbers as input 
+    parses the file and extracts valid phone numbers
+    
     Return valid phone numbers as output
     """
-    with open('phone_numbers.csv') as file:
-        reader = csv.reader(file, delimiter=' ')
-        line_count = 0 
-        # rex = '\d\d\d-\d\d\d-\d\d\d\d'
-        rex = r'\d[-]|((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}'
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        # line_count = 0 
+        rex = r'(?:\+1)?\d{3}-\d{3}-\d{4}|\d{3}\s*-\d{3}-\d{4}'
         for row in reader:
-            data = file.read()
-            match = findall(rex, data)
+            data = ' '.join(row)
+            matches = findall(rex, data)
+            for match in matches:
+                print(f"{match} is a valid phone number")
+        # if line_count == 0:
+        #     print(f'This is the matching data {','.join(row)}')
 
-    # print(match)
-        if line_count == 0:
-            print(f'This is the matching data {','.join(row)}')
+
+def main():
+    parser = argparse.ArgumentParser(description="""CLI tool for validating phone numbers. With the choice of individual emails, or a csv file.""")
+    parser.add_argument('-i', '--ind', help='Performs validation for the individual phone number given')
+    parser.add_argument('-f', '--file', help='Performs phone number validation and extraction for the csv given')
+
+    args = parser.parse_args()
+
+    if len(sys.argv) <2:
+        sys.exit("Usage: python3 phone_number_validator.py file\n\n"
+        "run python3 phone_number_validator.py -h for full help message")
+
+    if args.file:
+        phone_match(args.file)
 
 
 if __name__ == '__main__':
-    phone_match()
+    main()
